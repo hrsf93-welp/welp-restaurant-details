934692a2f5280db9eafae01619d643071b4ae260
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-04-12 19:46:25 -0700

    Reorganized file structure to use client and public folders

--- server/index.js
+++ server/index.js
@@ -5,6 +5,6 @@ const app = express();
 
 app.set('port', 3002);
 
-app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use(express.static(path.join(__dirname, '/../public')));
 
 app.listen(app.get('port'), console.log(`Listening on ${app.get('port')}`));
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client');
+const DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
   mode: 'development',
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     path: DIST_DIR,
-    filename: 'bundle.js'
+    filename: 'app.js',
   },
   module: {
     rules: [
       {
         test: /\.jsx?/,
+        exclude: /node_modules/,
         include: SRC_DIR,
         use: [
           {
-            loader: 'babel-loader', 
+            loader: 'babel-loader',
             query: {
-              presets: ['react', 'env']
-            }, 
-          }
-        ]
-      }
-    ]
-  }  
+              presets: ['react', 'env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
