fbacfb7255c7265c4f455f45be3cda674bd85fd0
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-04-14 10:39:55 -0700

    Fixed 1st Hours test

--- spec/hours.test.jsx
+++ spec/hours.test.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Hours from '../client/Hours';
+import EXAMPLE_RESTAURANT from '../data/exampleSingleEntryRestaurantInfo';
 
 describe('<Hours />', () => {
   it('renders seven <li> components', () => {
-    const wrapper = shallow(<Hours />);
+    const wrapper = mount(<Hours hours={EXAMPLE_RESTAURANT.hours} />);
     expect(wrapper.find('li').length).toBe(7);
   });
 });
