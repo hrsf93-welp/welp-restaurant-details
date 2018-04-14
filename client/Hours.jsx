36826f764246334754d9d529460a9ef0c60bb48d
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-04-14 15:53:06 -0700

    Renders a table for restaurant hours

--- client/Hours.jsx
+++ client/Hours.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
-class Hours extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false,
-    };
-  }
+// TODO: Print days in order
+// TODO: isBetween doesn't handle midnight correctly
+// TODO: lookup timezone using Google API and latitude/longitude
 
-  render() {
-    const openNow = this.state.isOpen ? (
-      <h4>Open Now</h4>
-    ) : (
-      <h4>Not Open</h4>
-    );
-
-    return (
-      <div>
-        {openNow}
-        <ul>
-          { Object.keys(this.props.hours).map(day => (
-            <li key={day}>{day}: {this.props.hours[day]}</li>))
+const Hours = props => (
+  <div>
+    <table>
+      <tbody>
+        { Object.keys(props.hours).map((day) => {
+          const formattedDay = day.charAt(0).toUpperCase() + day.slice(1);
+          const open = moment(props.hours[day].split('-')[0], 'HH:mm');
+          const close = moment(props.hours[day].split('-')[1], 'HH:mm');
+          let isOpen = '';
+          if (day === 'sunday') {
+            isOpen = moment('14:00', 'HH:mm').isBetween(open, close) ? 'Open Now' : 'Closed';
           }
-        </ul>
-      </div>
-    );
-  }
-}
+          return (
+            <tr key={day}>
+              <td>{formattedDay}</td>
+              <td>{`${open.format('h:mm a')} -${close.format('h:mm a')}`}</td>
+              <td>{isOpen}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  </div>
+);
 
 Hours.propTypes = {
   hours: PropTypes.shape({
--- spec/hours.test.jsx
+++ spec/hours.test.jsx
@@ -4,8 +4,8 @@ import Hours from '../client/Hours';
 import EXAMPLE_RESTAURANT from '../data/exampleSingleEntryRestaurantInfo';
 
 describe('<Hours />', () => {
-  it('renders seven <li> components', () => {
+  it('renders a row for each day of the week', () => {
     const wrapper = mount(<Hours hours={EXAMPLE_RESTAURANT.hours} />);
-    expect(wrapper.find('li').length).toBe(7);
+    expect(wrapper.find('tr').length).toBe(7);
   });
 });
