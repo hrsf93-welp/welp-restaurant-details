dd53748bec10bf8defbaafeb4c5584fe95696ddf
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-04-16 17:26:35 -0700

    Code cleanup for summary component code review

--- client/App.jsx
+++ client/App.jsx
@@ -19,35 +19,25 @@ class App extends React.Component {
       .then((response) => {
         this.setState({ restaurantInfo: response.data[0] });
         this.setState({ today: Today });
-      })
-      .catch((error) => {
-        console.log(error);
       });
   }
 
   render() {
-    const summary = this.state.restaurantInfo ? (
-      <Summary today={this.state.today} price={this.state.restaurantInfo.attributes.restaurantsPriceRange2} />
-    ) : (
-      <div>Summary not available</div>
-    );
+    const summary = this.state.restaurantInfo ?
+      <Summary today={this.state.today} price={this.state.restaurantInfo.attributes.restaurantsPriceRange2} /> :
+      <div>Summary not available</div>;
 
-    const hours = this.state.restaurantInfo ? (
-      <Hours hours={this.state.restaurantInfo.hours} />
-    ) : (
-      <div>Hours not available</div>
-    );
+    const hours = this.state.restaurantInfo ?
+      <Hours hours={this.state.restaurantInfo.hours} /> :
+      <div>Hours not available</div>;
 
-    const details = this.state.restaurantInfo.attributes ? (
-      <Details attributes={this.state.restaurantInfo.attributes} />
-    ) : (
-      <div>More Business Info not available</div>
-    );
+    const details = this.state.restaurantInfo ?
+      <Details attributes={this.state.restaurantInfo.attributes} /> :
+      <div>More Business Info not available</div>;
 
     return (
       <div>
         <h2>{this.state.restaurantInfo.name}</h2>
-        <h3>Summary</h3>
         {summary}
         <h3>Hours</h3>
         {hours}
