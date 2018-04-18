6b437ea2fca6c4dbf5f31c6995aeb4384892ed60
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-04-17 20:08:45 -0700

    Moved exampleData to test folder

--- spec/hours.test.jsx
+++ spec/hours.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { Hours } from '../client/Hours';
-import EXAMPLE_RESTAURANT from '../data/exampleSingleEntryRestaurantInfo';
+import EXAMPLE_RESTAURANT from './data/exampleSingleEntryRestaurantInfo';
 
 describe('<Hours />', () => {
   it('renders a row for each day of the week', () => {
