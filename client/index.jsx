426688e8dd0f98405c0ca7d1e182777aa02aa6be
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-04-24 11:16:44 -0700

    Updated index.jsx and index.html

--- client/index.jsx
+++ client/index.jsx
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/main.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('Details'));
